663fd108e525250fc71a8517a9380c97efeb2637
mirafgan <mirefqan1999@gmail.com>
2024-05-06 01:08:30 +0400

    content type

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const routes = require("./routes");
+app.use((req, res, next) => {
+    req.headers["content-type"] = "application/json"
+    next()
+})
 app.use(cors());
 app.use(express.json())
 app.use(routes)
